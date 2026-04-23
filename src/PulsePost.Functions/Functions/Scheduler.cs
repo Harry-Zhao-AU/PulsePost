f0bc14c2c3f85598eb1eef1bdf3acf2869424373
Harry-Zhao-AU <harry.zhao19@gmail.com>
2026-04-23 19:41:48 +1000

    Fix TimerTrigger cron to 6-field NCRONTAB format (0 0 22 * * 0)

--- src/PulsePost.Functions/Functions/Scheduler.cs
+++ src/PulsePost.Functions/Functions/Scheduler.cs
@@ -8,7 +8,7 @@ public class Scheduler(ILogger<Scheduler> logger)
 {
     [Function(nameof(Scheduler))]
     [ServiceBusOutput("fetch-topics", Connection = "SERVICE_BUS_CONNECTION")]
-    public string Run([TimerTrigger("0 22 * * 0")] TimerInfo timer)
+    public string Run([TimerTrigger("0 0 22 * * 0")] TimerInfo timer)
     {
         logger.LogInformation("Pipeline triggered at {Time}", DateTime.UtcNow);
         return "start";
